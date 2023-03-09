7cf84dae09b446fbf6ec63d53232cdff42886e85
anupzpt <prajapatianu433@gmail.com>
2023-03-09 20:57:24 +0545

    patterns

---
+++ src/Pattern3.java
@@ -0,0 +1,11 @@
+public class Pattern3 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+
+        }
+    }
+}
